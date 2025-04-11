2d98dbd9377afd7f7e2ec6328d435513becb5da6
Dmytro Myronenko <dim@uain.su>
2025-04-11 21:06:02 +0300

    fixed start_BE

--- 4_start_BE.bat
+++ 4_start_BE.bat
@@ -1,13 +1,17 @@
 @echo off
+setlocal
+
+set "BASEDIR=%~dp0"
 
 echo [cs2_webradar] Checking if "usermode/release/usermode.exe" file exists...
 
-if exist "usermode/release/usermode.exe" (
-    cd usermode/release
+if exist "%BASEDIR%usermode\release\usermode.exe" (
     echo [cs2_webradar] Starting usermode...
-    start usermode/release/usermode.exe
+    start "" "%BASEDIR%usermode\release\usermode.exe"
 ) else (
-    echo [cs2_webradar] failed to start usermode.exe! 
+    echo [cs2_webradar] Failed to start usermode.exe!
     echo Make sure you compiled usermode.hpp via VisualStudio!
     pause
 )
+
+endlocal
